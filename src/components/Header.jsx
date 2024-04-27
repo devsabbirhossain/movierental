c8002678a8fa3fe643d80515312a42bd9786acae
devsabbirhossain <sabbir2dev@gmail.com>
2024-04-27 16:46:26 +0600

    add cart details

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,41 +1,58 @@
+import { useState } from "react";
 import moon from "../assets/icons/moon.svg";
 import logo from "../assets/logo.svg";
 import ring from "../assets/ring.svg";
 import shoppingCart from "../assets/shopping-cart.svg";
+import CartDetails from "./cart/CartDetails";
 
 const Header = () => {
-  return (
-    <header>
-      <nav className="container flex items-center justify-between space-x-10 py-6">
-        <a href="index.html">
-          <img src={logo} width="139" height="26" alt="" />
-        </a>
+  const [ShowCart, setShowCart] = useState(false);
 
-        <ul className="flex items-center space-x-5">
-          <li>
-            <a
-              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
-              href="#">
-              <img src={ring} width="24" height="24" alt="" />
-            </a>
-          </li>
-          <li>
-            <a
-              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
-              href="#">
-              <img src={moon} width="24" height="24" alt="" />
-            </a>
-          </li>
-          <li>
-            <a
-              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
-              href="#">
-              <img src={shoppingCart} width="24" height="24" alt="" />
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </header>
+  function handleSeeCartDetails() {
+    event.preventDefault();
+    setShowCart(true);
+  }
+
+  function handleCloseCartDetails(){
+    event.preventDefault();
+    setShowCart(false);
+  }
+  return (
+    <>
+      {ShowCart && <CartDetails onClose={handleCloseCartDetails}/>}
+      <header>
+        <nav className="container flex items-center justify-between space-x-10 py-6">
+          <a href="index.html">
+            <img src={logo} width="139" height="26" alt="" />
+          </a>
+
+          <ul className="flex items-center space-x-5">
+            <li>
+              <a
+                className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+                href="#">
+                <img src={ring} width="24" height="24" alt="" />
+              </a>
+            </li>
+            <li>
+              <a
+                className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+                href="#">
+                <img src={moon} width="24" height="24" alt="" />
+              </a>
+            </li>
+            <li>
+              <a
+                onClick={handleSeeCartDetails}
+                className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+                href="#">
+                <img src={shoppingCart} width="24" height="24" alt="" />
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 };
 
